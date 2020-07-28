b3e67a21888a1ff65111210fa8e505976b9b38e0
DillonBarker <barker.dill98@gmail.com>
2020-07-28 16:36:57 +0100

    ready for stage 9

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -3,8 +3,8 @@ require 'pg'
 class Bookmark
 
   def self.all
-    connection = PG.connect(dbname: 'bookmark_manager')
-    print = connection.exec('SELECT * FROM bookmarks;')
+    conn = PG.connect(dbname: 'bookmark_manager')
+    print = conn.exec('SELECT * FROM bookmarks;')
     print.map { |bookmark| bookmark['url'] }
   end
 
